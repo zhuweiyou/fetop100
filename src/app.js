a644374eca64e1b9ef3c317cbbdc1340bb763999
zhuweiyou <i@zhuweiyou.com>
2017-11-20 10:37:56 +0800

    reformat

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 App({
-    onLaunch () {
+  onLaunch () {
 
-    }
+  }
 })
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,31 +1,31 @@
 import Rank from '../../service/rank'
 
 Page({
-    data: {
-        rankList: []
-    },
-    onLoad () {
-        this.getData()
-    },
-    getData () {
-        Rank.getList(rankList => {
-            this.setData({rankList})
-            wx.stopPullDownRefresh()
-        })
-    },
-    onPullDownRefresh () {
-        this.getData()
-    },
-    tapRankItem (event) {
-        const index = event.currentTarget.dataset.index
-        const rankList = this.data.rankList
-        rankList[index].open = !rankList[index].open
-        this.setData({rankList})
-    },
-    onShareAppMessage () {
-        return {
-            title: 'TOP100的前端框架排名',
-            path: '/page/index/index'
-        }
+  data: {
+    rankList: []
+  },
+  onLoad () {
+    this.getData()
+  },
+  getData () {
+    Rank.getList(rankList => {
+      this.setData({rankList})
+      wx.stopPullDownRefresh()
+    })
+  },
+  onPullDownRefresh () {
+    this.getData()
+  },
+  tapRankItem (event) {
+    const index = event.currentTarget.dataset.index
+    const rankList = this.data.rankList
+    rankList[index].open = !rankList[index].open
+    this.setData({rankList})
+  },
+  onShareAppMessage () {
+    return {
+      title: 'TOP100的前端框架排名',
+      path: '/page/index/index'
     }
+  }
 })
