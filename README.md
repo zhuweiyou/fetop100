1e097a0e3188169ba8514bc4ee8a6b29fc6b8164
zhuweiyou <i@zhuweiyou.com>
2017-11-20 09:29:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# fetop100
+
+“前端TOP100”小程序（AppID wx993aa7f068f791ea）
+
+数据来源 https://www.awesomes.cn/rank?sort=trend
+
+线上小程序码 | 本地开发截图
+--- | ---
+<img src="wacode.jpg" alt="线上小程序码"> | <img width="320" src="screenshot.png" alt="本地开发截图">|
---
+++ src/service/rank.js
@@ -0,0 +1,26 @@
+export default {
+    getList (cb) {
+        const retry = () => setTimeout(() => this.getList(cb), 3000)
+        wx.request({
+            url: 'https://www.awesomes.cn/rank?sort=trend',
+            header: {
+                'accept-language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+                'content-type': 'text/html; charset=utf-8'
+            },
+            success: ({data}) => {
+                try {
+                    data = JSON.parse(data.match(/"data":\[\{"repos":(.*?),"sort":"trend"/).pop()).map(item => ({
+                        id: item.id,
+                        icon: `https://awesomes.oss-cn-beijing.aliyuncs.com/repo/${item.cover}?x-oss-process=style/subject_repo`,
+                        name: item.name,
+                        detail: item.description_cn
+                    }))
+                    cb(data)
+                } catch (e) {
+                    retry()
+                }
+            },
+            fail: retry
+        })
+    }
+}
