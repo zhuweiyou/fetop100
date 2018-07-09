f5ea4be1c9a7921b9ddd1b269500289454838ced
zhuweiyou <i@zhuweiyou.com>
2018-07-09 16:16:13 +0800

    使用自带 loading

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -3,24 +3,30 @@ import Rank from '../../service/rank'
 Page({
   data: {
     rankList: [],
+    githubUrl: 'https://github.com/zhuweiyou/fetop100',
   },
   onLoad() {
     this.getData()
   },
+  onPullDownRefresh() {
+    this.getData()
+  },
   getData() {
+    wx.showNavigationBarLoading()
     Rank.getList(rankList => {
       this.setData({ rankList })
       wx.stopPullDownRefresh()
+      wx.hideNavigationBarLoading()
     })
-  },
-  onPullDownRefresh() {
-    this.getData()
   },
   tapRankItem(event) {
     const index = event.currentTarget.dataset.index
     const rankList = this.data.rankList
     rankList[index].open = !rankList[index].open
     this.setData({ rankList })
+  },
+  copyGithubUrl() {
+    wx.setClipboardData({ data: this.data.githubUrl })
   },
   onShareAppMessage() {
     return {
