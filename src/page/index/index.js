7179454a030939ce3ae27663a2e12f9cfed638da
zhuweiyou <i@zhuweiyou.com>
2019-04-24 12:29:29 +0800

    awesomes 页面更新，重写解析逻辑

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,37 +1,37 @@
-import Rank from '../../service/rank'
+import Rank from "../../service/rank";
 
 Page({
   data: {
     rankList: [],
-    githubUrl: 'https://github.com/zhuweiyou/fetop100',
+    githubUrl: "https://github.com/zhuweiyou/fetop100"
   },
   onLoad() {
-    this.getData()
+    this.getData();
   },
   onPullDownRefresh() {
-    this.getData()
+    this.getData();
   },
   getData() {
-    wx.showNavigationBarLoading()
+    wx.showNavigationBarLoading();
     Rank.getList(rankList => {
-      this.setData({ rankList })
-      wx.stopPullDownRefresh()
-      wx.hideNavigationBarLoading()
-    })
+      this.setData({ rankList });
+      wx.stopPullDownRefresh();
+      wx.hideNavigationBarLoading();
+    });
   },
   tapRankItem(event) {
-    const index = event.currentTarget.dataset.index
-    const rankList = this.data.rankList
-    rankList[index].open = !rankList[index].open
-    this.setData({ rankList })
+    const index = event.currentTarget.dataset.index;
+    const rankList = this.data.rankList;
+    rankList[index].open = !rankList[index].open;
+    this.setData({ rankList });
   },
   copyGithubUrl() {
-    wx.setClipboardData({ data: this.data.githubUrl })
+    wx.setClipboardData({ data: this.data.githubUrl });
   },
   onShareAppMessage() {
     return {
-      title: 'TOP100的前端框架排名',
-      path: '/page/index/index',
-    }
-  },
-})
+      title: "TOP100的前端框架排名",
+      path: "/page/index/index"
+    };
+  }
+});
--- src/service/rank.js
+++ src/service/rank.js
@@ -1,26 +1,33 @@
 export default {
   getList(cb) {
-    const retry = () => setTimeout(() => this.getList(cb), 3000)
+    const retry = () => setTimeout(() => this.getList(cb), 3000);
     wx.request({
-      url: 'https://www.awesomes.cn/rank?sort=trend',
+      url: "https://www.awesomes.cn/rank?sort=trend",
       header: {
-        'accept-language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
-        'content-type': 'text/html; charset=utf-8',
+        "accept-language": "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4",
+        "content-type": "text/html; charset=utf-8"
       },
       success: ({ data }) => {
         try {
-          data = JSON.parse(data.match(/"data":\[\{"repos":(.*?),"sort":"trend"/).pop()).map(item => ({
-            id: item.id,
-            icon: `https://awesomes.oss-cn-beijing.aliyuncs.com/repo/${item.cover}?x-oss-process=style/subject_repo`,
-            name: item.name,
-            detail: item.description_cn,
-          }))
-          cb(data)
+          const list = data.match(
+            /<div class="list-item">([\s\S]*?)<\/span>[\s\S]*?<\/div>[\s\S]*?<\/div>/g
+          );
+          if (!list || !list.length) {
+            return retry();
+          }
+          cb(
+            list.reduce((array, item) => {
+              const [all, icon, name, detail] = item.match(
+                /<img src="([\s\S]*?)"[\s\S]*?<h4>([\s\S]*?)<\/h4>[\s\S]*?<span class="sdesc">([\s\S]*?)<\/span>/
+              );
+              return [...array, { icon, name, detail }];
+            }, [])
+          );
         } catch (e) {
-          retry()
+          retry();
         }
       },
-      fail: retry,
-    })
-  },
-}
+      fail: retry
+    });
+  }
+};
